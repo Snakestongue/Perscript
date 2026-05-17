fe18e20c761ca98c17d74484047e2bceb375c83c
Snakestongue <maeher.grover@gmail.com>
2026-05-16 21:29:04 -0400

    CSS Updates

--- FRONTEND/src/App.jsx
+++ FRONTEND/src/App.jsx
@@ -209,15 +209,19 @@ function App(){
           <p class="titles">Excercise</p>
             {problems.slice(0, 3).map((p)=>(
             <button class="sidebars"
-              key={p.id}onClick={()=>handleProblemChange({target:{value:  p.id}})}>
-              {p.title}
+              key={p.id}onClick={()=>handleProblemChange({target:{value:  p.id}})}>•  {p.title}
             </button> 
             ))}
           <button
             id="AK"
             class="sidebars"
             onClick={()=>setUserCode(selectedProblem.solutionCode[currentLang])}>
-            Show Correct Answer
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-2">
+              <path stroke-linecap="round" stroke-linejoin="round" d="M12 18v-5.25m0 0a6.01 6.01 0 0 0 1.5-.189m-1.5.189a6.01 6.01 0 0 1-1.5-.189m3.75 7.478a12.06 12.06 0 0 1-4.5 0m3.75 2.383a14.406 14.406 0 0 1-3 0M14.25 18v-.192c0-.983.658-1.823 1.508-2.316a7.5 7.5 0 1 0-7.517 0c.85.493 1.509 1.333 1.509 2.316V18" />
+            </svg>
+
+            
+             Show Correct Answer
           </button>
           <button id="submit" class="sidebars" onClick={handleSubmit}>▶ RUN & SUBMIT</button>
           <p class="sidebars" id="feedback">{feedback}</p>
